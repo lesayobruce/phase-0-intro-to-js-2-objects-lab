eb9cf7b3b9a4dc4a5d896ce446a1df805de2e4a9
mashughuli <lesayobruce419@gmail.com>
2022-12-04 06:54:37 +0300

    completed the test

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var employee = {} ;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    for (const key in employee) {
+        delete employee[key];
+      }
+
+      employee.name = 'Sam';
+   let newEmployee = {...employee};
+   delete newEmployee.name;
+   return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    for (const key in employee) {
+        delete employee[key];
+      }
+
+      employee.name = 'Sam';
+      delete employee.name
+      return employee
+}
+console.log(destructivelyDeleteFromEmployeeByKey());
